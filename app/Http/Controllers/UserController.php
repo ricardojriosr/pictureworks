42a183487c3b6f72b570425b876ac938db8d70ed
Ricardo Rios <ricardojriosr@gmail.com>
2020-05-24 21:48:41 -0400

    Final Fixes after testing with Postman

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller {
 
     // Get User By It's ID
     public function getUserById($id) {
-        if (!is_numeric($id)) { return response()->json('Invalid id', 422); }
+        if (!is_numeric($id)) { return response()->json('Invalid ID', 422); }
         $userData = User::find($id);
         if (empty($userData)) { return response()->json('User Does Not Exist!', 404); }
         return view('user', ['user' => $userData]);
@@ -19,12 +19,13 @@ class UserController extends Controller {
     public function update(Request $request) {
         if (Helper::isJson($request->getContent())) {
             $requestData = json_decode($request->getContent(), 1);
+            print_r($requestData);
         } else {
             return response()->json('Invalid POST JSON', 422);
         }
         foreach(['password', 'id', 'comments'] as $key){
             if($this->missing_post($requestData,$key) or !$key)
-                return response()->json('Missing key/value for "'.$key.'"', 422);
+                return response()->json('Missing Value for "'.$key.'"', 422);
         }
         if(strtoupper($requestData['password']) != '720DF6C2482218518FA20FDC52D4DED7ECC043AB') { return response()->json('Invalid password', 401); }
         if (!is_numeric($requestData['id'])) { return response()->json('Invalid ID', 422); }
@@ -38,8 +39,12 @@ class UserController extends Controller {
     public function updateData($id, $comments, $password = '720DF6C2482218518FA20FDC52D4DED7ECC043AB') {
         $userData = User::find($id);
         if (empty($userData)) { return response()->json('User Does Not Exist!', 404); }
-        $userData->update(['comments' => $userData->comments . '' . $comments]);
+        $userData->update(['comments' => $userData->comments . '<br>' . $comments]);
         return response()->json('OK', 200);
+    }
+
+    function missing_post($requestedData,$field){
+        return (!isset($requestedData[$field]) or !$requestedData[$field]);
     }
 
 }
